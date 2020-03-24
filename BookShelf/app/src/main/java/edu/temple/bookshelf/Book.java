ad91d416ca48f3e48807d705acca8dcd4171a0f0
Ayanna Woodlin <tuf39609@temple.edu>
2020-03-23 20:01:24 -0400

    added author and title string arrays

--- BookShelf/app/src/main/java/edu/temple/bookshelf/Book.java
+++ BookShelf/app/src/main/java/edu/temple/bookshelf/Book.java
@@ -1,4 +1,27 @@
 package edu.temple.bookshelf;
 
+import java.util.HashMap;
+
 public class Book {
+    private String author;
+    private String title;
+
+    private HashMap<String, String> bookCollection;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+
+        bookCollection.put(author, title);
+
+    }
+
+    public HashMap GetCollection() {
+        return bookCollection;
+    }
+
+    public void addToCollection(String author, String title) {
+        bookCollection.put(author, title);
+    }
+
 }
